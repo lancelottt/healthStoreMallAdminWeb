5daa3ed4de97624877732071a4a02baf72bc6361
xueyicui5431 <17611732208@163.com>
2019-04-20 16:10:36 +0800

    配置项合并

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://39.98.69.210:8080"'
+    NODE_ENV: '"production"',
+    BASE_API: '"http://2b3665801a.51mypc.cn:20626"'
 }
